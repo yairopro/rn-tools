492708015601a83b0b2d7bfe9d95d08c8c0edf21
yair <yairopro@gmail.com>
2020-07-30 11:37:35 +0300

    View responsive levels

--- component/View.js
+++ component/View.js
@@ -5,14 +5,37 @@ import use from "../hook";
 
 export default function View({ responsive, ...props }) {
 	const [layout, setLayout] = use.state(null);
-	if (responsive)
+	if (responsive) {
+		if (responsive instanceof Array) {
+			const levelsSeparators = responsive.map(Number)
+				.filter(width => width > 0);
+
+			if (levelsSeparators.length > 0) // render only when level change
+				responsive = ({ width }, { width: oldWidth }) => {
+					const oldIndex = levelsSeparators.findIndex(separator => oldWidth < separator);
+					const newIndex = levelsSeparators.findIndex(separator => width < separator);
+					return oldIndex !== newIndex;
+				};
+
+			else // no valid levels
+				responsive = () => false;
+		}
+
+		if (!(responsive instanceof Function))
+			responsive = () => true; // always render
+
 		props.onLayout = parallel(
 			props.onLayout,
-			event => setLayout(event.nativeEvent.layout),
+			event => {
+				const newLayout = event.nativeEvent.layout;
+				if (!layout || responsive(newLayout, layout))
+					setLayout(newLayout);
+			},
 		);
+	}
 
 	if (props.children instanceof Function)
-			props.children = props.children({layout});
+		props.children = props.children({ layout });
 
 	return <RNView {...props} />;
 }
