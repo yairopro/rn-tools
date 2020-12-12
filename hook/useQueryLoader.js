e14a4cf21dc9e5ab00db68567ca79420a5b912d8
Yair <yairopro@gmail.com>
2020-12-12 20:00:39 +0200

    fix

--- hook/useQueryLoader.js
+++ hook/useQueryLoader.js
@@ -1,24 +1,7 @@
-import { useLazyQuery } from "@apollo/client"
-import { useSyncEffect } from "./useEffect";
+import { useQuery, gql } from "@apollo/client"
 
 export default function useQueryLoader(query, variables) {
-	const [load, loader] = useLazyQuery(query, { variables });
-	useSyncEffect(() => {
-		if (query) {
-			load();
-			loader.loading = true;
-		}
-	}, [query]);
-
-	loader.load = (query && load) || null;
-
-	return loader;
+	return useQuery(query || EMPTY_QUERY, { variables, skip: !query });
 }
 
-/**
- * @typedef Loader
- * @property data {* | undefined}
- * @property load {Function | undefined} Function to load next page or reload in case of error. load() is undefined once all data is loaded
- * @property loading {boolean}
- * @property error {* | undefined}
- */
+const EMPTY_QUERY = gql`{_}`;
