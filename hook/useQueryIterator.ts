7e2db14a63eba3730ffea164b2ee40f3314af226
Yair <yairopro@gmail.com>
2021-02-11 22:17:13 +0200

    useQueryIterator

---
+++ hook/useQueryIterator.ts
@@ -0,0 +1,96 @@
+import { useState } from "react"
+import { QueryLoader } from './useQueryLoader';
+import useMemory from "./useMemory"
+import { Subject } from "rxjs";
+import useSubscribe from "./useSubscribe"
+
+export default function useQueryIterator<TData, TVariables extends object>(
+	loader: QueryLoader<TData, TVariables>,
+	config: QueryIteratorConfig<TData>,
+): QueryIterator<TData, TVariables> {
+	const iterator = useMemory(() => new QueryIterator(loader, config), [loader]);
+	useSubscribe(iterator?.subscribe, [iterator]);
+	return iterator;
+}
+
+export interface QueryIteratorConfig<TData> {
+	retrieveItems: (data: TData) => any[] | undefined,
+	range?: number,
+}
+
+export class QueryIterator<TData, TVariables> {
+	loader: QueryLoader<TData, TVariables>;
+	config: QueryIteratorConfig<TData>;
+
+	fetching = false;
+	end = false;
+	observable = new Subject();
+
+	constructor(
+		loader: QueryLoader<TData, TVariables>,
+		config: QueryIteratorConfig<TData>,
+	) {
+		this.loader = loader;
+		this.config = config;
+	}
+
+	get loading() {
+		return this.fetching || this.loader.loading;
+	}
+
+	get items() {
+		const data = this.loader?.data;
+		if (data)
+			return this.config.retrieveItems(data);
+	}
+
+	willLoadNext(variables: object): () => Promise<any[]> | undefined {
+		if (!this.end && !this.loading)
+			return () => this.next(variables);
+	}
+
+	next(variables: object): Promise<any> | undefined {
+		if (this.loading || this.end)
+			return;
+
+		this.fetching = true;
+		this.observable.next(this);
+
+		const { loader, config } = this;
+		const promise = loader.fetchMore({
+			variables: Object.assign({ ...loader.variables }, variables),
+		}).then(result => {
+			if (result.data)
+				return config.retrieveItems(result.data);
+
+			const graphQLError = result.error || result.errors?.[0];
+			if (graphQLError)
+				throw Object.assign(new Error(graphQLError.message), graphQLError);
+
+			const error = new Error("QueryIterator.next: Unknown error, see error.result");
+			// @ts-ignore
+			error.result = result;
+			throw result;
+		});
+
+		// check if end is reached
+		promise.then(addedItems => {
+			const addedLength = addedItems?.length;
+			this.end = Number(config?.range) >= 0 ?
+				addedLength < config.range
+				: !(addedLength >= 0);
+		})
+			// clean state
+			.finally(() => {
+				this.fetching = false;
+				this.observable.next(this);
+			});
+
+		return promise;
+	}
+
+	subscribe = (callback) => {
+		const subscription = this.observable.subscribe(callback);
+		return () => subscription.unsubscribe();
+	}
+}
--- hook/useQueryLoader.ts
+++ hook/useQueryLoader.ts
@@ -3,12 +3,19 @@ import { useQuery, gql, QueryResult, DocumentNode, TypedDocumentNode } from "@ap
 function useQueryLoader<TData, TVariables>(
 	query: DocumentNode | TypedDocumentNode<any, TVariables>,
 	variables: TVariables
-): QueryResult<TData, TVariables> {
-	const loader = useQuery(query || EMPTY_QUERY, { variables, skip: !query });
-	loader.loading = query /* undefined-able */ && loader.loading;
+): QueryLoader<TData, TVariables> {
+	const loader = useQuery(query || EMPTY_QUERY, { variables, skip: !query }) as QueryLoader<TData, TVariables>;
+	loader.variables = variables; // used by the query iterator
+	loader.loading = !query ? undefined /* <-- loading must =undefined if no query. 
+				It allows components to set their default value when deconstructing the loader */
+		: loader.loading;
 	return loader;
 }
 
 export default useQueryLoader;
 
 const EMPTY_QUERY = gql`{_}`;
+
+export interface QueryLoader<TData, TVariables> extends QueryResult<TData, TVariables> {
+	variables: TVariables,
+}
